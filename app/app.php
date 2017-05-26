f95a23a11d3bbbdc52ba6d48f65e5c416b0f1425
Minh Phuong <mphuong@kent.edu>
2017-05-26 11:33:43 -0700

    initialize Silex template

--- app/app.php
+++ app/app.php
@@ -3,6 +3,12 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/RepeatCounter.php";
 
+  //Initialize app and template
+  $app = new Silex\Application();
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+
 
 });
 
