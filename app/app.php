e3fe6933cd35affa2b44101d6e452802ab514cd6
Minh Phuong <mphuong@kent.edu>
2017-05-26 11:44:58 -0700

    add content to index.php

--- app/app.php
+++ app/app.php
@@ -1,31 +1,31 @@
 <?php
-//set up modules
-require_once __DIR__."/../vendor/autoload.php";
-require_once __DIR__."/../src/RepeatCounter.php";
+  //set up modules
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/RepeatCounter.php";
 
-//Initialize app and template
-$app = new Silex\Application();
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/../views'
-));
+  //Initialize app and template
+  $app = new Silex\Application();
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
 
-//Get input route
-$app->get("/", function() use ($app){
-  return $app['twig']->render('input.html.twig'));
-});
+  //Get input route
+  $app->get("/", function() use ($app){
+    return $app['twig']->render('input.html.twig'));
+  });
 
-//Post output route
-$app->post("/output", function() use ($app){
-  $wordCounter = new RepeatCounter();
-  $sentence = $_POST['sentence'];
-  $word = $_POST['word'];
-  $lowerCase = $wordCounter->toLower($sentence);
-  $split = $wordCounter->splitString($lowerCase);
-  $result = $wordCounter->wordCount($split,$word);
+  //Post output route
+  $app->post("/output", function() use ($app){
+    $wordCounter = new RepeatCounter();
+    $sentence = $_POST['sentence'];
+    $word = $_POST['word'];
+    $lowerCase = $wordCounter->toLower($sentence);
+    $split = $wordCounter->splitString($lowerCase);
+    $result = $wordCounter->wordCount($split,$word);
 
-  return $app['twig']->render('output.html.twig', array('sentence'=>$sentence, 'word'=>$word,'result' => $result));
-});
+    return $app['twig']->render('output.html.twig', array('sentence'=>$sentence, 'word'=>$word,'result' => $result));
+  });
 
-return $app;
+  return $app;
 
 ?>
