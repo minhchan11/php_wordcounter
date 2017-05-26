7fe951208de3558a0dd20db4ca8ac7646a7fc8b2
Minh Phuong <mphuong@kent.edu>
2017-05-26 11:18:25 -0700

    addwordCount

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -11,6 +11,16 @@ class RepeatCounter
     return $split_str= explode(" ",$lowerSentence);
   }
 
-  
+  function wordCount($split_str,$word)
+  {
+    $counter = 0;
+    for ($i=0; $i < count($split_str) ; $i++) {
+      if ($split_str[$i]== $word) {
+        $counter++;
+      }
+    }
+    return $counter;
+  }
+
 }
 ?>
