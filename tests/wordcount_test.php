8bbea6b7f2da00890ed9c6fc101cf66032286a6f
Minh Phuong <mphuong@kent.edu>
2017-05-26 11:23:05 -0700

    add first test for toLower

---
+++ tests/wordcount_test.php
@@ -0,0 +1,17 @@
+<?php
+  require_once "src/RepeatCounter.php";
+
+  class wordcount_test extends PHPUnit_Framework_TestCase
+  {
+    function test_toLowerSentence()
+    {
+      $testCounter = new RepeatCounter();
+      $sentence = "Hello How Are You?";
+
+      $expected = "hello how are you?";
+      $actual = $testCounter->toLower($sentence);
+
+      $this->assertEquals($expected,$actual);
+    }
+  }
+ ?>
